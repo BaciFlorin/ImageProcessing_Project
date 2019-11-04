25d8d9e1f04fe197679c7cc3a43783b5d39cc749
Sergiu Corban <sergiucorban@gmail.com>
2019-11-04 23:10:52 +0200

    Initial commit

---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_Browse_clicked();
+
+    void on_pushButton_Exit_clicked();
+
+    void on_pushButton_Next_clicked();
+
+    void on_lineEdit_Browse_editingFinished();
+
+private:
+    Ui::MainWindow *ui;
+    QString file_path;
+    QString file_path_filter;
+};
+#endif // MAINWINDOW_H
